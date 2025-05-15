36a32f3492b689c0adbb39d968f2bac6f0de1c68
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-05-15 19:15:42 +0200

    Update admin.py

--- plugins/database/admin.py
+++ plugins/database/admin.py
@@ -6,7 +6,7 @@ from pyrogram.types import (
 )
 from plugins.config import Config
 from pyrogram import Client, enums
-from plugins.database.database import db
+from plugins.database.database import db  # Ensure this is imported for database access
 from plugins.functions.display_progress import humanbytes
 from pyrogram import Client
 
@@ -35,33 +35,5 @@ async def status_handler(_, m: Message):
              f"**CPU Usage:** {cpu_usage}% \n"
              f"**RAM Usage:** {ram_usage}%\n\n"
              f"**Total Users in DB:** `{total_users}`",
-        quote=True
-    )
-
-
-@Client.on_message(filters.command('broadcast') & filters.user(Config.OWNER_ID))
-async def broadcast_handler(client, message: Message):
-    if len(message.command) < 2:
-        await message.reply_text("Usage: /broadcast <message>", quote=True)
-        return
-    
-    broadcast_message = message.text.split(None, 1)[1]
-    total_users = 0
-    sent_count = 0
-    failed_count = 0
-
-    async for user in db.get_all_users():
-        total_users += 1
-        try:
-            await client.send_message(chat_id=user['id'], text=broadcast_message)
-            sent_count += 1
-        except Exception:
-            failed_count += 1
-
-    await message.reply_text(
-        text=f"Broadcast completed.\n\n"
-             f"**Total Users:** {total_users}\n"
-             f"**Messages Sent:** {sent_count}\n"
-             f"**Failed:** {failed_count}",
         quote=True
     )
